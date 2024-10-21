599beae4be4c44ef738c3f33dbc44faff1b316f9
mukta81 <168319607+mukta81@users.noreply.github.com>
2024-10-21 14:17:00 +0200

    Add files via upload
    Updated review comments

--- tests/day02/homework/gradeCalculation.js
+++ tests/day02/homework/gradeCalculation.js
@@ -1,47 +1,51 @@
-let studentScore = 75;
+let studentScore = 1020;
 gradeCalculation(studentScore);
 
 function gradeCalculation(score){
-console.log(score)
+  if (score < 0 || score > 100) {
+    console.log("Valid Range of score is 0 to 100");
+    return;
+  }
+
   switch(true){
-    case (score>=0 && score<20):{
+    case (score>=0 && score<20):
       console.log("Student's grade is F")
       break;
-    }
+    
 
-    case (score>=20 && score<40):{
+    case (score>=20 && score<40):
       console.log("Student's grade is E")
       break;
-    }
+    
 
-    case (score>=40 && score<60):{
+    case (score>=40 && score<60):
       console.log("Student's grade is D")
       break;
-    }
+    
 
-    case (score>=60 && score<75):{
+    case (score>=60 && score<75):
       console.log("Student's grade is C")
       break;
-    }
+    
 
-    case (score>=75 && score<90):{
+    case (score>=75 && score<90):
       console.log("Student's grade is B")
       break;
-    }
+    
 
-    case (score>=90 && score<100):{
+    case (score>=90 && score<100):
       console.log("Student's grade is A")
       break;
-    }
+    
 
-    case (score==100):{
+    case (score==100):
       console.log("Student's grade is A+")
       break;
-    }
+    
 
-    default:{
+    default:
       console.log("Valid Range of score is 0 to 100")
-    }
+    
 
   }
 }
