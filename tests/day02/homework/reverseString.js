e42eda0f310c692bd9b3237abe94500e99427d40
mukta81 <168319607+mukta81@users.noreply.github.com>
2024-10-23 00:03:59 +0200

    Update reverseString.js
    Fixed the code

--- tests/day02/homework/reverseString.js
+++ tests/day02/homework/reverseString.js
@@ -3,21 +3,20 @@ let revString = getReverseString(myString);
 checkPalindrome(myString, revString)
 
 function getReverseString(str) {
-  console.log("inside getReverseString " + str)
-  str = str.split("")
-  return "hello" //temporary
+  revStr='';
+  strArr = str.split("")
+  for (let index = strArr.length-1; index >=0; index--) {
+    revStr=revStr+strArr[index]
+  }
+  //console.log(revStr)
+  return revStr;
 }
 
 function checkPalindrome(str, revStr) {
-  console.log("inside palindrome " + str +","+revStr)
-  if (str == revStr) {
+   if (str.toLowerCase().replaceAll(" ","") === revStr.toLowerCase().replaceAll(" ","")) {
     console.log(str + ": is a palindrome")
   }
   else {
     console.log(str + ": is not a palindrome")
   }
 }
-
-
-
-
