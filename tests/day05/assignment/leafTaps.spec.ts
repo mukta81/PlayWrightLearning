ad530baadc9ffae609ceb721b3f80478ec9fef88
mukta81 <168319607+mukta81@users.noreply.github.com>
2024-11-02 10:40:23 +0100

    Add files via upload

---
+++ tests/day05/assignment/leafTaps.spec.ts
@@ -0,0 +1,41 @@
+import {test} from "playwright/test";
+
+test('LeafTaps', async({page})=>{
+//open application
+await page.goto("http://leaftaps.com/opentaps/control/login");
+
+//fill login details
+await page.locator("#username").fill("demosalesmanager");
+await page.locator("#password").fill("crmsfa");
+
+//click on login button
+await page.locator(".decorativeSubmit").click();
+
+//Click on CRM/SFA link
+await page.locator('text="CRM/SFA"').click();
+
+//Click on Leads
+await page.locator('text="Leads"').click();
+
+//Click on Create Lead
+await page.locator('text="Create Lead"').click();
+
+//enter mandatory details
+await page.locator("#createLeadForm_companyName").fill("Testleaf");
+await page.locator("#createLeadForm_firstName").fill("Mukta");
+await page.locator("#createLeadForm_lastName").fill("Agarwal");
+
+//trying to select value of dropdown with different ways
+await page.selectOption("#createLeadForm_dataSourceId", {index: 1})
+await page.waitForTimeout(2000);
+await page.selectOption("#createLeadForm_dataSourceId", {value: "LEAD_CONFERENCE"})
+await page.waitForTimeout(2000);
+await page.selectOption("#createLeadForm_dataSourceId", {label: "Direct Mail"})
+
+//click submit
+await page.locator(".smallSubmit").click();
+
+
+
+
+})
